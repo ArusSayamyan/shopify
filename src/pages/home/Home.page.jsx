d352dd54beb2c0bc4223d3d9382c794e402bd636
Пользователь <arus.sayamyan@gmail.com>
2021-04-02 22:57:38 +0400

    Created Loader component

--- src/pages/home/Home.page.jsx
+++ src/pages/home/Home.page.jsx
@@ -3,34 +3,42 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import CategoryItem from "src/pages/home/components/category-item/CategoryItem.component";
 
+import Loader from "src/components/loader/Loader.component";
 // STYLES
 import styles from "./home.module.scss";
 
 const Home = (props) => {
-  const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get("categories");
-        setCategories(response.data);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    getData();
-  }, []);
+    useEffect(() => {
+        const getData = async () => {
+            setIsLoading(true);
+            try {
+                const response = await axios.get("categories");
+                setCategories(response.data);
+            } catch (error) {
+                console.log(error.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        getData();
+    }, []);
 
-  const sortedCategories = categories.sort((a, b) => a.id - b.id);
+    const sortedCategories = categories.sort((a, b) => a.id - b.id);
 
-  console.log(props);
-  return (
-    <ul className={styles.container}>
-      {sortedCategories.map((category) => {
-        return <CategoryItem key={category._id} category={category} />;
-      })}
-    </ul>
-  );
+    return isLoading ? (
+        <div className={styles.loaderWrapper}>
+            <Loader />
+        </div>
+    ) : (
+        <ul className={styles.container}>
+            {sortedCategories.map((category) => {
+                return <CategoryItem key={category._id} category={category} />;
+            })}
+        </ul>
+    );
 };
 
 export default Home;
